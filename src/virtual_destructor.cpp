9003ec4395734678011ee27833b687e530a3eeae
Yusuf Etkin KIZILDAĞ <49615906+yusufetkinkizildag@users.noreply.github.com>
2022-10-20 00:09:10 +0300

    Update virtual_destructor.cpp

--- src/virtual_destructor.cpp
+++ src/virtual_destructor.cpp
@@ -5,6 +5,7 @@ class NonVirtualBase
 public:
     NonVirtualBase() { std::cout << "NonVirtualBase constructor" << std::endl; }
     ~NonVirtualBase() { std::cout << "NonVirtualBase destructor" << std::endl; }
+    void show() { std::cout << "NonVirtualBase show" << std::endl; }
 };
 
 class VirtualBase
@@ -12,6 +13,7 @@ class VirtualBase
 public:
     VirtualBase() { std::cout << "VirtualBase constructor" << std::endl; }
     virtual ~VirtualBase() { std::cout << "VirtualBase destructor" << std::endl; }
+    virtual void show() { std::cout << "VirtualBase show" << std::endl; }
 };
 
 class Derived1 : public NonVirtualBase
@@ -19,6 +21,7 @@ class Derived1 : public NonVirtualBase
 public:
     Derived1() { std::cout << "Derived1 constructor" << std::endl; }
     ~Derived1() { std::cout << "Derived1 destructor" << std::endl; }
+    void show() { std::cout << "Derived1 show" << std::endl; }
 };
 
 class Derived2 : public VirtualBase
@@ -26,17 +29,27 @@ class Derived2 : public VirtualBase
 public:
     Derived2() { std::cout << "Derived2 constructor" << std::endl; }
     ~Derived2() { std::cout << "Derived2 destructor" << std::endl; }
+    void show() { std::cout << "Derived2 show" << std::endl; }
 };
 
 int main(int argc, char const *argv[])
 {
-    std::cout << std::endl;
     NonVirtualBase *var1{new Derived1{}};
+    Derived1 * var2{new Derived1{}};
+    var1->show();
+    var2->show();
     delete var1;
+    delete var2;
+
     std::cout << std::endl;
 
-    VirtualBase *var2{new Derived2{}};
-    delete var2;
+    VirtualBase *var3{new Derived2{}};
+    Derived2 * var4{new Derived2{}};
+    var3->show();
+    var4->show();
+    delete var3;
+    delete var4;
+
     std::cout << std::endl;
 
     return 0;
